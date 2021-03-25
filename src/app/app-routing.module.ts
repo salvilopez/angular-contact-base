71548e974fede4a9f11963fefebf85c876616e38
Salvador Sierra <salvilopezronda@gmail.com>
2021-03-25 19:06:03 +0100

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+/**
+ * Rutas de la aplicacion
+ */
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch: 'full',
+    redirectTo:'/home'
+  }
+  ,
+{
+  path:'home',
+  component: HomePageComponent
+},
+{
+  path:'login',
+  component: LoginPageComponent
+}
+,
+{
+  path:'register',
+  component: RegisterPageComponent
+}, {
+  path:'**',
+  component: NotFoundPageComponent
+}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
+import {  ReactiveFormsModule } from '@angular/forms';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    ContactComponent
+    ContactComponent,
+    LoginFormComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     // We import HttpClientModule to perform http requests
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
