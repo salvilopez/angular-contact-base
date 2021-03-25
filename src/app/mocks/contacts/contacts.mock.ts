7710b50b26dacb6bb8d74dd1d516cfb447b10a8a
Martín San José de Vicente <martin@Martins-MacBook.home>
2021-03-25 13:05:33 +0100

    Mock de Contactos creado

--- src/app/mocks/contacts/contacts.mock.ts
+++ src/app/mocks/contacts/contacts.mock.ts
@@ -1,1 +1,16 @@
 import { Contact } from '../../models/contact/contact.model';
+
+export const CONTACTS: Contact[] = [
+  {
+    first_name: 'Martín',
+    last_name: 'San José',
+    email: 'martin@imaginagroup.com',
+    phone: '768920422'
+  },
+  {
+    first_name: 'Andrés',
+    last_name: 'San José',
+    email: 'andres@imaginagroup.com',
+    phone: '876678992'
+  }
+]
--- src/app/models/contact/contact.model.ts
+++ src/app/models/contact/contact.model.ts
@@ -1,12 +1,28 @@
+import { IContact } from "./icontact.interface";
+export class Contact implements IContact{
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
 
-
-
-export class Contact implements Contact{
-  // first_name: string;
-
-  constructor(name: string ) {
-
+  constructor(first_name: string, last_name: string, email: string, phone:string ) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.phone = phone;
   }
+
+  /**
+   * showDetails()
+   * @returns string
+   * Contact details in string
+   */
+  // showDetails(): string {
+  //   return `Contact: ${this.first_name} ${this.last_name}
+  //   \nEmail: ${this.email}
+  //   \nPhone:${this.phone}
+  //   `
+  // }
 
 
 }
